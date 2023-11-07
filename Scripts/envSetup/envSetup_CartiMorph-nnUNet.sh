4356bf7d0f382b117eda89623c2113b4af9a9ca5
YongchengYAO <yao_yongcheng@icloud.com>
2023-11-07 16:13:25 +0000

    update: change pytorch-cuda=11.7 to pytorch-cuda=12.1

--- Scripts/envSetup/envSetup_CartiMorph-nnUNet.sh
+++ Scripts/envSetup/envSetup_CartiMorph-nnUNet.sh
@@ -3,6 +3,9 @@
 Dir_current="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 Dir_parent="${Dir_current%/*}"
 
+# remove old Conda environment: CartiMorphToolbox-nnUNet 
+conda remove --name CartiMorphToolbox-nnUNet --all -y 
+
 # create Conda environment
 conda create -n CartiMorphToolbox-nnUNet -y
 
@@ -14,7 +17,7 @@ conda activate CartiMorphToolbox-nnUNet
 conda install python=3.10 -y
 
 # PyTorch
-conda install pytorch torchvision torchaudio pytorch-cuda=11.7 -c pytorch -c nvidia -y
+conda install pytorch torchvision torchaudio pytorch-cuda=12.1 -c pytorch -c nvidia -y
 
 # CartiMorph-nnUnet
 python -m pip install CartiMorph-nnUnet
